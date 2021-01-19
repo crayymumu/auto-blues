448c9da0ba43f8b620ba36afca5187bcbca4e915
zhuyangsong <854994301@qq.com>
2021-01-19 09:34:00 +0800

    feat: add resize function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
 
 <script lang="ts">
 import Harmonica from '@/components/Harmonica.vue'
-import { reactive, toRefs } from 'vue'
+import { reactive, toRefs, onBeforeMount } from 'vue'
 
 export default {
   components: {
@@ -16,6 +16,35 @@ export default {
     const state = reactive({
       value: 123
     })
+
+    const resize = () => {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      const $wrapper = document.getElementById('app')
+      let style = '';
+      if (width >= height) { // 横屏
+        style += 'width:' + width + 'px;'; // 注意旋转后的宽高切换
+        style += 'height:' + height + 'px;';
+        style += '-webkit-transform: rotate(0); transform: rotate(0);';
+        style += '-webkit-transform-origin: 0 0;';
+        style += 'transform-origin: 0 0;';
+      } else { // 竖屏
+        style += 'width:' + height + 'px;';
+        style += 'height:' + width + 'px;';
+        style += '-webkit-transform: rotate(90deg); transform: rotate(90deg);';
+        // 注意旋转中点的处理
+        style += '-webkit-transform-origin: ' + width / 2 + 'px ' + width / 2 + 'px;';
+        style += 'transform-origin: ' + width / 2 + 'px ' + width / 2 + 'px;';
+      }
+      // eslint-disable-next-line
+      ($wrapper as any).style.cssText = style;
+    }
+
+    onBeforeMount(() => {
+      window.addEventListener('resize', resize)
+      resize()
+    })
+
     return {
       ...toRefs(state),
     }
